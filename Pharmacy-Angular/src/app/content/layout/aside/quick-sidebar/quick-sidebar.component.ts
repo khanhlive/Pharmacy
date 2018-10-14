0f1da0f7c6d5f9c75cad16f9046721d5fa963f7f
khanhlive <khanhngdinh@outlook.com>
2018-10-15 06:09:37 +0700

    INIT
    Khởi tạo project

---
+++ Pharmacy-Angular/src/app/content/layout/aside/quick-sidebar/quick-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'm-quick-sidebar-action',
+  templateUrl: './quick-sidebar.component.html',
+  styleUrls: ['./quick-sidebar.component.scss']
+})
+export class QuickSideBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
